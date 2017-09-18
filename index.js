07898ecf9e830d1cf6a6149540d28d22413956f1
yesI <1317154549@qq.com>
2017-09-18 02:24:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,25 +18,29 @@ function destructivelyRemoveFirstKitten() {
   return kittens.shift();
 }
 
-function appendKitten(name) {
-  var k = kittens;
-  k.push(name)
-  return k;
-}
+
 
 function prependKitten(name) {
   var a = [name].concat(kittens);
   return a;
 }
 
+
+
+function appendKitten(name) {
+  var k = kittens.slice();
+  k.push(name)
+  return k;
+}
 function removeLastKitten() {
-  var a = kittens;
+  var a = kittens.slice();
   a.pop()
   return a;
 }
 
 function removeFirstKitten() {
-  let a = kittens;
+  let a = kittens.slice();
   a.shift();
   return kittens;
 }
+
